bfe10776087dad441a3bb486785de721bdb3402b
jarek3660-cmd <jarek3660@gmail.com>
2026-04-09 12:37:45 -0600

    Update TenStateCounter1.vhdl

--- TenStateCounter1.vhdl
+++ TenStateCounter1.vhdl
@@ -1,87 +1,74 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 04/08/2026 09:12:11 PM
--- Design Name: 
--- Module Name: TenStateCounter - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity TenStateCounter is
-    Port ( Clk_Sys, Reset, Direction : in STD_LOGIC;
-           Count : out STD_LOGIC_VECTOR (3 downto 0));
+    Port (
+        Clk_Sys   : in  STD_LOGIC;
+        Reset     : in  STD_LOGIC;
+        Direction : in  STD_LOGIC;
+        Count     : out STD_LOGIC_VECTOR(3 downto 0)
+    );
 end TenStateCounter;
 
 architecture Behavioral of TenStateCounter is
 
-type state_type is(s0, s1, s2, s3, s4, s5, s6, s7, s8, s9);
-
-signal presentState, nextState : state_type;
+    type state_type is (s0, s1, s2, s3, s4, s5, s6, s7, s8, s9);
+    signal presentState, nextState : state_type;
 
 begin
 
-syncProcess : process (Clk_Sys, Reset)
+    syncProcess : process (Clk_Sys, Reset)
     begin
-        if Reset = '1' then 
+        if Reset = '1' then
             presentState <= s0;
         elsif rising_edge(Clk_Sys) then
             presentState <= nextState;
         end if;
     end process;
-    
-    combinatoralProcess : process (presentState, Direction)
-begin
-    case presentState is
-        when s0 => Cathode_7SD <= "00000011";
-                 if Direction = '1' then nextState <= s1;
-                else nextState <= s9;
+
+    nextStateProcess : process (presentState, Direction)
+    begin
+        case presentState is
+            when s0 =>
+                if Direction = '1' then
+                    nextState <= s1;
+                else
+                    nextState <= s9;
                 end if;
-        when s1 => Cathode_7SD <= "10011111";
-            nextState <= s2;   
-        when s2 => Cathode_7SD <= "00100101";
-            nextState <= s3;
-        when s3 => Cathode_7SD <= "00001101";
-            nextState <= s4;
-        when s4 => Cathode_7SD <= "10011001";
-            nextState <= s0;
-         
-        when s9 => Cathode_7SD <= "00001001";
-             if Direction = '0' then nextState <= s8;
-                else nextState <= s0;
-            end if;
-        when s8 => Cathode_7SD <= "00000001";
-            nextState <= s7;
-        when s7 => Cathode_7SD <= "00011111";
-            nextState <= s6;
-        when s6 => Cathode_7SD <= "01000001";
-            nextState <= s5;
-        when s5 => Cathode_7SD <= "01001001";
-            nextState <= s9;
-    end case;
-end process;
+
+            when s1 => nextState <= s2;
+            when s2 => nextState <= s3;
+            when s3 => nextState <= s4;
+            when s4 => nextState <= s0;
+
+            when s9 =>
+                if Direction = '0' then
+                    nextState <= s8;
+                else
+                    nextState <= s0;
+                end if;
+
+            when s8 => nextState <= s7;
+            when s7 => nextState <= s6;
+            when s6 => nextState <= s5;
+            when s5 => nextState <= s9;
+        end case;
+    end process;
+
+    countProcess : process (presentState)
+    begin
+        case presentState is
+            when s0 => Count <= "0000";
+            when s1 => Count <= "0001";
+            when s2 => Count <= "0010";
+            when s3 => Count <= "0011";
+            when s4 => Count <= "0100";
+            when s5 => Count <= "0101";
+            when s6 => Count <= "0110";
+            when s7 => Count <= "0111";
+            when s8 => Count <= "1000";
+            when s9 => Count <= "1001";
+        end case;
+    end process;
 
 end Behavioral;
